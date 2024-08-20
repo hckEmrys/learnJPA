824d61abf9068b75d7f0604469672929ae0f8eb8
Arun MD <arun.dabbe@venmatics.com>
2024-08-20 14:06:17 +0530

    Initial commit

---
+++ src/main/java/com/learning/jpa/learnJPA/LearnJpaApplication.java
@@ -0,0 +1,13 @@
+package com.learning.jpa.learnJPA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learning/jpa/learnJPA/LearnJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.jpa.learnJPA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
